1bcede2e7a2cfe138eca14aa02bdf2cc3d5d0b0e
MNikhil0210 <nikhilpavansai.machavaram@zemosolabs.com>
2020-10-02 16:10:00 +0530

    feat: added basic landing page with header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import smoothScroll from "smoothscroll-polyfill";
+import Header from "./Components/Header/Header.js";
+import Home from "./Components/Home/Home.js";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    smoothScroll.polyfill();
+
+    const getJsonData = async () => {
+      await fetch(`${process.env.PUBLIC_URL}/my_data.json`)
+        .then((data) => data.json())
+        .then((data) => setData(data));
+    };
+    getJsonData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className="section__header">Basic portfolio</p>
-      </header>
+    <div className="app">
+      <Header logo={data.logo} name={data.name} />
+      <Home
+        name={data.name}
+        background={data.backgrounds}
+        about={data.about}
+      />
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./Header.css";
+
+function Header({ logo, name }) {
+  const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
+  const [isTop, setIsTop] = useState(true);
+  const navLinksRef = useRef(null);
+  const firstTimeUpdate = useRef(true);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setIsTop(window.scrollY < 30);
+    });
+
+    document.body.addEventListener(
+      "click",
+      (e) => {
+        if (!e.target.classList.contains("hamburger")) {
+          setIsHamburgerOpen(false);
+        }
+      },
+      false
+    );
+  }, []);
+
+  useEffect(() => {
+    if (firstTimeUpdate.current) {
+      firstTimeUpdate.current = false;
+      return;
+    }
+
+    const navLinks = document.querySelector(".nav-links");
+    if (isHamburgerOpen) {
+      navLinks.classList.add("show__navLinks");
+    } else {
+      navLinks.classList.remove("show__navLinks");
+    }
+  }, [isHamburgerOpen]);
+
+  return (
+    <div
+      className="header"
+      style={
+        isTop
+          ? { backgroundColor: "transparent" }
+          : { backgroundColor: "black" }
+      }
+    >
+      <a href="/" className="logo">
+        <img src={`${process.env.PUBLIC_URL}/${logo}`} alt="" />
+        <h1>{name?.firstName}</h1>
+      </a>
+      <ul className="nav-links" ref={navLinksRef}>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Resume</li>
+        <li>Works</li>
+        <li>Contact</li>
+      </ul>
+      <div
+        className="hamburger"
+        onClick={(e) => setIsHamburgerOpen(!isHamburgerOpen)}
+      >
+        <div className="hamburger__top"></div>
+        <div className="hamburger__middle"></div>
+        <div className="hamburger__bottom"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
